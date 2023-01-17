081ff863a9db4c45339a268e198886ea1f0dda49
DMS055 <thebest755p@gmail.com>
2023-01-17 22:49:00 +0100

    Category Class Structure

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,20 @@
-﻿namespace ExpenseTrackerDOTNET.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTrackerDOTNET.Models
 {
     public class Category
     {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "navchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "navchar(5)")]
+        public string Icon { get; set; }
+
+        [Column(TypeName = "navchar(10)")]
+        public string Type { get; set; }
     }
 }
