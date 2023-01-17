0629d5846e7ffa656b097ea47fa02a7511e00da1
DMS055 <thebest755p@gmail.com>
2023-01-17 22:51:32 +0100

    Add Transaction Class

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,6 +1,10 @@
-﻿namespace ExpenseTrackerDOTNET.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTrackerDOTNET.Models
 {
     public class Transaction
     {
+        [Key]
+        public int TransactionId { get; set; }
     }
 }
