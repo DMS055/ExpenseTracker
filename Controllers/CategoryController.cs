225b1eeb47716ab76acb11abdd021e58ae5770ce
DMS055 <thebest755p@gmail.com>
2023-01-22 00:36:36 +0100

    Add Menu Grid

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -60,7 +60,10 @@ namespace ExpenseTrackerDOTNET.Controllers
         {
             if (ModelState.IsValid)
             {
+                if(category.CategoryId == 0)
                 _context.Add(category);
+                else
+                    _context.Update(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
