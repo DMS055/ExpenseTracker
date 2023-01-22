9bc0bc6edee169519d46350ff327b383cc4589ad
DMS055 <thebest755p@gmail.com>
2023-01-22 03:34:04 +0100

    Add Icon To Grid

--- Models/Category.cs
+++ Models/Category.cs
@@ -16,5 +16,14 @@ namespace ExpenseTrackerDOTNET.Models
 
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon+ " " +this.Title;
+            }
+        }
     }
 }
